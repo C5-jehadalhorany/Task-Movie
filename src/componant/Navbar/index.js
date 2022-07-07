98159c89ea600c52f75ac9260a2e5038215d701d
jehadalhorany <alhoranyjehad@gmail.com>
2022-07-07 11:56:44 +0300

    add deitales for movie

--- src/componant/Navbar/index.js
+++ src/componant/Navbar/index.js
@@ -1,17 +1,30 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, Container, Nav, Image } from 'react-bootstrap'
+import { Navbar, Container, Nav, Image, Button, Badge } from 'react-bootstrap'
 import("./style.css")
 
 
 
 const NavbaR = () => {
+
+
+
     return <Navbar bg="secondary" variant="dark"  >
+
         <Container className='primary'>
             <Navbar.Brand href="/" className='home4'> Movie</Navbar.Brand>
+
+
             <Nav className="me-left ">
-                <Nav.Link href="#home" className='home1'>Home</Nav.Link>
+
+                {/* <Nav.Link href="#home" className='home1'>Home</Nav.Link> */}
+
                 <Nav.Link href="" className='home2'></Nav.Link>
                 <Nav.Link href="" className='home3'></Nav.Link>
+                <button variant="primary">
+                    My Fav <Badge bg="secondary"></Badge>
+                    <span className="visually-hidden"></span>
+                </button>
+
             </Nav>
         </Container>
     </Navbar >

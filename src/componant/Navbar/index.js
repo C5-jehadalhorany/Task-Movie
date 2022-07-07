351fb62d5a070b446e7d5143023e54156c909494
jehadalhorany <alhoranyjehad@gmail.com>
2022-07-08 00:36:27 +0300

    and navbar

--- src/componant/Navbar/index.js
+++ src/componant/Navbar/index.js
@@ -1,22 +1,38 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Favorites from '../favorites';
 import { Navbar, Container, Nav, Image, Button, Badge } from 'react-bootstrap'
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from 'react';
 import("./style.css")
 
 
 
 const NavbaR = () => {
+ const navigate=useNavigate()
 
+ const favState = useSelector((state) => {
+    return {
 
-    let arr= []
+        fav: state.fav.fav,
+
+    };
+});
+
+useEffect(()=>{
+    
+},[favState.fav])
+
+let a= favState.fav.length
     return <Navbar bg="secondary" variant="dark"  >
         <Container className='primary'>
             <Navbar.Brand href="/" className='home4'> Movie</Navbar.Brand>
             <Nav className="me-left ">
                 {/* <Nav.Link href="#home" className='home1'>Home</Nav.Link> */}
                 <Nav.Link href="" className='home2'></Nav.Link>
-                <Nav.Link href="" className='home3'></Nav.Link>
-                <button variant="primary">
-                    My Fav <Badge bg="secondary">{arr.length}</Badge>
+                <Nav.Link href="/favorites" className='home3'></Nav.Link>
+                <button className='button_navbar' variant="primary" onClick={()=>{navigate("/favorites")}} >
+                    My Fav <Badge bg="secondary">{a}</Badge>
                     <span className="visually-hidden"></span>
                 </button>
             </Nav>

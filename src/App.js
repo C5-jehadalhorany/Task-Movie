8de9bdba1e6b82a7a28d2a6cb0ae1880a7da8647
jehadalhorany <alhoranyjehad@gmail.com>
2022-07-07 14:27:21 +0300

    Eidt for app

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import NavbaR from './componant/Navbar';
 import Movie from './componant/Movie';
 import { Routes, Route, Link } from "react-router-dom";
 import MovieDetiles from './componant/Details';
+import Popup from './componant/popup';
+import bootstrap from 'bootstrap'
 import './App.css';
 
 function App() {
     return (<div>
         <NavbaR/>
+        
         <Routes>
         <Route path='/' element={ <Movie/>} />
         <Route path='/details/:id' element={<MovieDetiles />} />

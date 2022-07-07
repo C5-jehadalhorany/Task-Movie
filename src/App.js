e33af0b260a47361126edbaf26996735c1f5b2f3
jehadalhorany <alhoranyjehad@gmail.com>
2022-07-08 00:29:56 +0300

    app add componant

--- src/App.js
+++ src/App.js
@@ -4,19 +4,31 @@ import Movie from './componant/Movie';
 import { Routes, Route, Link } from "react-router-dom";
 import MovieDetiles from './componant/Details';
 import Popup from './componant/popup';
-import bootstrap from 'bootstrap'
+import bootstrap from 'bootstrap';
+import React, { useState, createContext, useEffect } from "react";
+import Favorites from './componant/favorites';
 import './App.css';
+export const tokenContext = createContext();
 
 function App() {
+    const [fav, setfav] = useState((JSON.parse(localStorage.getItem("fav")) || []))
     return (<div>
-        <NavbaR/>
+
+        <NavbaR />
         
-        <Routes>
-        <Route path='/' element={ <Movie/>} />
-        <Route path='/details/:id' element={<MovieDetiles />} />
-        </Routes>
-        
+        <tokenContext.Provider value={{ fav, setfav }}>
+
+            <Routes>
+                <Route path='/' element={<Movie />} />
+                <Route path='/details/:id' element={<MovieDetiles />} />
+                <Route path='/favorites' element={<Favorites />} />
+            </Routes>
+
+        </tokenContext.Provider>
+
     </div>)
 }
+
+
 
 export default App;

91c5e1a44ff3e8895ddbda0622562bcef0570546
jehadalhorany <alhoranyjehad@gmail.com>
2022-07-07 14:29:21 +0300

    Eidt for navbar

--- src/componant/Navbar/index.js
+++ src/componant/Navbar/index.js
@@ -7,24 +7,18 @@ import("./style.css")
 const NavbaR = () => {
 
 
-
+    let arr= []
     return <Navbar bg="secondary" variant="dark"  >
-
         <Container className='primary'>
             <Navbar.Brand href="/" className='home4'> Movie</Navbar.Brand>
-
-
             <Nav className="me-left ">
-
                 {/* <Nav.Link href="#home" className='home1'>Home</Nav.Link> */}
-
                 <Nav.Link href="" className='home2'></Nav.Link>
                 <Nav.Link href="" className='home3'></Nav.Link>
                 <button variant="primary">
-                    My Fav <Badge bg="secondary"></Badge>
+                    My Fav <Badge bg="secondary">{arr.length}</Badge>
                     <span className="visually-hidden"></span>
                 </button>
-
             </Nav>
         </Container>
     </Navbar >
